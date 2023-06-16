79255b229afccdeaca96e7bb3b08fa545116e7c4
Vigersand <kenny.vigersand@zitac.se>
2023-06-16 15:57:42 +0200

    Made changes to Credentials class

--- Zitac.VmWare/Classes/Credentials.cs
+++ Zitac.VmWare/Classes/Credentials.cs
@@ -1,18 +1,19 @@
-using DecisionsFramework.Design.Flow;
-using DecisionsFramework.Design.Properties;
 using DecisionsFramework.Design.Properties.Attributes;
 using DecisionsFramework.Design.ConfigurationStorage.Attributes;
 using DecisionsFramework.Design.Flow.Service.Debugging.DebugData;
-using System;
-using System.Collections.Generic;
+using System.Runtime.Serialization;
+
 
 namespace Zitac.VmWare.Steps;
 
+        [DataContract]
         public class Credentials : IDebuggerJsonProvider
     {
+        [DataMember]
         [WritableValue]
         public string? Username { get; set; }
 
+        [DataMember]
         [WritableValue]
         [PasswordText]
         public string? Password { get; set; }
